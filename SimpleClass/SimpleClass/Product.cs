113957c507329a26751fa77f2436f26dc1ea9945
Rohitashav Aggarwal <837967@ACDM.DS.SAIT.CA>
2019-12-16 11:49:30 -0700

    day 4 c# practice

---
+++ SimpleClass/SimpleClass/Product.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    public class Product
+    {
+        // private data
+        private string name;
+        private decimal price;
+        private int qty;
+
+        // public properties
+        public string Name
+        {
+            get { return name;}
+            set
+            {
+                name = value; // use the value provided when property is used
+            }
+        }
+        public decimal Price
+        {
+            get{ return price; }
+            set
+            {
+                // if provided value is negative, make it zero
+                price = (value < 0) ? -value : value;
+            }
+        }
+        public int Qty
+        {
+            get { return qty; }
+            set
+            {
+                // if provided value is negative, make it zero
+                qty = (value < 0) ? -value : value;
+            }
+        }
+
+
+        // constructors
+        // if you do not provide a constructor, compiler supplies default 
+        //  constructor (no parameters)
+        public Product() { } // default constructor
+        public Product(string n, decimal p, int q) // constructor with params
+        {
+            Name = n;
+            Price = p;
+            Qty = q;    
+        }
+
+        // public operations
+
+        public override string ToString()
+        {
+            return name + ": " + price.ToString("c") + ", " + qty.ToString();
+        }
+
+    }
+}
---
+++ SimpleClass/SimpleClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1; // defines reference variable
+            p1 = new Product(); // create an object - 
+                                //implicit call to default constructor
+            p1.Name = "gadget";
+            p1.Price = 9.99m;// use set part of property Price
+            //Console.WriteLine("The price is " + p1.Price.ToString("c")); // uses get part of property Price
+            p1.Qty = 15;
+            
+            Console.WriteLine(p1);// implicit call to ToString() method
+
+            Product p2 = new Product("widget", -7.99m, -25); // implicit call to
+                                                            // constructor with params
+            Console.WriteLine(p2);
+
+
+            Console.WriteLine("\nPress any key to finish");
+            Console.ReadKey();
+        }
+    }
+}
