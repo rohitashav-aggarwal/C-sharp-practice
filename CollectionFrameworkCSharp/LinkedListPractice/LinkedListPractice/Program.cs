96bbeda9dc922cfad3949aa35e0cd600b9b04793
Rohitashav Aggarwal <837967@ACDM.DS.SAIT.CA>
2020-01-15 13:51:48 -0700

    linked list update

--- CollectionFrameworkCSharp/LinkedListPractice/LinkedListPractice/Program.cs
+++ CollectionFrameworkCSharp/LinkedListPractice/LinkedListPractice/Program.cs
@@ -11,7 +11,45 @@ namespace LinkedListPractice
         static void Main(string[] args)
         {
             // LinkedList: items are accessed sequentially instead of using an index
+            string[] songs = { "song1", "song2","song3", "song4","song4", "song6", "song7"};
 
+            // create a linkedlist that holds strings
+            LinkedList<string> mylist = new LinkedList<string>(songs);
+
+            // items can be added to the front or the back of the list
+            mylist.AddFirst("song0");
+            mylist.AddLast("song8");
+
+            // linkedlist can be interated
+            foreach(string s in mylist)
+            {
+                Console.WriteLine(s);
+            }
+
+            // First and last properties return linkedlist nodes
+            LinkedListNode<string> first = mylist.First;
+            LinkedListNode<string> last = mylist.Last;
+            Console.WriteLine(first.Value);
+            Console.WriteLine(last.Value);
+
+            // items can be added or removed relative to an existing item
+            mylist.AddAfter(last, "song9");
+            mylist.AddBefore(first, "song00");
+            Console.WriteLine("**********************************");
+            foreach (string s in mylist)
+            {
+                Console.WriteLine(s);
+            }
+
+            // search for items in the list with contains and find
+            Console.WriteLine("**********************************");
+            Console.WriteLine(mylist.Contains("song10"));
+            Console.WriteLine(mylist.Find("song00").Value);
+            // the list can then be travered with those properties
+            Console.WriteLine(first.Next.Value);
+            Console.WriteLine(last.Previous.Value);
+            //
+            Console.ReadKey();
         }
     }
 }
